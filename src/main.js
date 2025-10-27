128623c6f52b8604896e36801a2582d6e3eb9f3f
이수경 <isuekyoung@Sues-Macbook-Pro.local>
2025-10-27 20:35:28 +0900

    fix

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#button');
+const timeBar= document.querySelector('#timebar');
+const timeFill = document.querySelector('#time');
+const bug = document.querySelector('#bug');
+
+const text1 = document.querySelector('#textArea1');
+const text2 = document.querySelector('#textArea1');
